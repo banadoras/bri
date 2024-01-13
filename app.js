3a9ff3cf443d1504d0e20965861887539c78d766
Joseph Hanna <banadoras@yahoo.com>
2024-01-13 14:52:01 -0500

    v1

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.get("/:password",(req,res)=>{
+    const password = req.params.password
+    if(password === "1234"){
+        res.send(require("./data"))
+    }else{
+        res.send([])
+    }
+})
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log("Listening to port 3000")
+})
---
+++ data.js
@@ -0,0 +1,10 @@
+
+
+const list = [
+    {id:0,name:"Tom",grade:1},
+    {id:1,name:"Sam",grade:4},
+    {id:2,name:"Karen",grade:6},
+    {id:3,name:"Pam",grade:2},
+]
+
+module.exports = list
